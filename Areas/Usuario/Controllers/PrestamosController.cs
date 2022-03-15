54ca5510d837440f0c06250c4a7f520cfb460e28
Cristobal2019 <cristobalcruzzarate@gmail.com>
2022-03-14 18:20:45 -0600

    listado de Prestamos

---
+++ Areas/Usuario/Controllers/PrestamosController.cs
@@ -0,0 +1,22 @@
+﻿using CristobalCruz.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CristobalCruz.Areas.Usuario.Controllers
+{
+    [Area("Usuario")]
+    public class PrestamosController : Controller
+    {
+
+        private readonly MyConexionBD _context;
+        public PrestamosController( MyConexionBD context)
+        {           
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Prestamo.ToList());
+        }
+    }
+}
