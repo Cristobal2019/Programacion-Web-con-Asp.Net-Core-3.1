416abd5fb019a4fee8d7b875df9e68b6b360fd47
Cristobal2019 <cristobalcruzzarate@gmail.com>
2022-03-23 11:17:05 -0600

    Reorganizacion de tabla responsive

--- wwwroot/js/misJsProyecto.js
+++ wwwroot/js/misJsProyecto.js
@@ -62,29 +62,19 @@ $(".verPrestamo").click(function () {
     toastr.error(data.message);
 })
 //----------------------------------------------------------------------------------------
-    $(document).ready(function () {
-        //Llamar a datatable
-        $('#tblPrestamos,#tblRecibos,#TablaUsuario').DataTable({
-            "lengthMenu":[[5,-1],[5,"ALL"]],
-            language: {
-                "decimal": "",
-                "emptyTable": "No hay información",
-                "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
-                "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
-                "infoFiltered": "(Filtrado de _MAX_ total entradas)",
-                "infoPostFix": "",
-                "thousands": ",",
-                "lengthMenu": "Mostrar _MENU_ Entradas",
-                "loadingRecords": "Cargando...",
-                "processing": "Procesando...",
-                "search": "Buscar:",
-                "zeroRecords": "Sin resultados encontrados",
-                "paginate": {
-                    "first": "Primero",
-                    "last": "Ultimo",
-                    "next": "Siguiente",
-                    "previous": "Anterior"
-                }
-            }
-        });
+
+$(function () {
+    $('#tblCliente,#tblPrestamo').DataTable({
+        "paging": true,
+        "lengthChange": true,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "responsive": true,
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
+        },
+        "lengthMenu": [[5, -1], [5, "ALL"]]
     });
+});
